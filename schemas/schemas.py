5f7b5d54b9ea518d0a45a55f4f55bca38df010fb
Arystan <akarto95@gmail.com>
2026-05-06 11:17:46 +0500

    chore: schemas

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,1 +1,39 @@
+POST_SCHEMA = {
+    "type": "object",
+    "required": ["id", "title", "body", "userId"],
+    "properties": {
+        "id":     {"type": "integer"},
+        "title":  {"type": "string"},
+        "body":   {"type": "string"},
+        "userId": {"type": "integer"},
+    },
+    "additionalProperties": False
+}
 
+USER_SCHEMA = {
+    "type": "object",
+    "required": ["id", "name", "username", "email", "address", "phone", "website", "company"],
+    "properties": {
+        "id":       {"type": "integer"},
+        "name":     {"type": "string"},
+        "username": {"type": "string"},
+        "email":    {"type": "string"},
+        "address":  {"type": "object"},
+        "phone":    {"type": "string"},
+        "website":  {"type": "string"},
+        "company":  {"type": "object"},
+    }
+}
+
+COMMENT_SCHEMA = {
+    "type": "object",
+    "required": ["id", "postId", "name", "email", "body"],
+    "properties": {
+        "id":     {"type": "integer"},
+        "postId": {"type": "integer"},
+        "name":   {"type": "string"},
+        "email":  {"type": "string"},
+        "body":   {"type": "string"},
+    },
+    "additionalProperties": False
+}
